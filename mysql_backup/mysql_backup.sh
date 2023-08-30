0fe9d878eef21c651666c4b6cec14426437d6f4a
Bojan <el.vujke@gmail.com>
2023-08-30 18:46:26 +0200

    Update mysql_backup.sh

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -1,22 +1,33 @@
 #! /bin/bash
 
+#: Title		: Backup all MySQL Databases
+#: Date			: 2014
+#: Author		: github.com/vujke
+#: Version		: 1.0
+#: Description	: 
+
 DATE=$(date +"%d%m%Y%H%M")
 
 MYSQL=/usr/bin/mysql
 MYSQLDUMP=/usr/bin/mysqldump
 MYSQL_HOST="localhost"
-MYSQL_USERNAME="vujke"
+MYSQL_USERNAME=""
 MYSQL_PASSWORD=""
-# DATABASE_NAME=""
-
-BACKUP_DIR="$HOME/backup/mysql"
+BACKUP_DIR="$HOME/Downloads/mysqldump"
 DATABASE_DIR="$BACKUP_DIR/$DATABASE_NAME"
+SQL_FILE="$DATABASE_NAME"
 
 # Check if database directory exist
 [ ! -d "$BACKUP_DIR" ]  && mkdir -p $BACKUP_DIR
 
-DATABASES=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
+# List all databases
+# SHOW DATABASES; is regular MySQL command that shows all databases
+# Command grep -Ev will NOT show defined database(s)
+LIST_OF_DATABASES=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys|mysql)"`
 
-for DATABASE_NAME in $DATABASES; do
-  $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $DATABASE_NAME --skip-lock-tables | gzip > "$BACKUP_DIR/$([ ! -d "$BACKUP_DIR/$DATABASE_NAME" ]  && mkdir -p $BACKUP_DIR/$DATABASE_NAME)/$DATABASE_NAME"_"$DATE.gz"
+for DATABASE_NAME in $LIST_OF_DATABASES; do
+  $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $DATABASE_NAME --skip-lock-tables > ${BACKUP_DIR}/${DATABASE_NAME}_${DATE}.sql
+  gzip -q $BACKUP_DIR/*
 done
+
+echo "${LIST_OF_DATABASES}" | wc -l
