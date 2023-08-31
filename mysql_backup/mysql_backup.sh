6d4a2900d7e23bd806505c0408bea279e5622627
Bojan <el.vujke@gmail.com>
2023-08-31 10:30:01 +0200

    Update mysql_backup.sh

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -21,13 +21,13 @@ SQL_FILE="$DATABASE_NAME"
 [ ! -d "$BACKUP_DIR" ]  && mkdir -p $BACKUP_DIR
 
 # List all databases
-# SHOW DATABASES; is regular MySQL command that shows all databases
+# SHOW DATABASES is regular MySQL command that shows all databases
 # Command grep -Ev will NOT show defined database(s)
 LIST_OF_DATABASES=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys|mysql)"`
 
 for DATABASE_NAME in $LIST_OF_DATABASES; do
   $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $DATABASE_NAME --skip-lock-tables > ${BACKUP_DIR}/${DATABASE_NAME}_${DATE}.sql
-  # Compressing every database
+  # Compress every database
   gzip -q $BACKUP_DIR/*
 done
 
