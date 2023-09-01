0a1b0eab797baa2cff7f2fa895664d81c6e383d2
Bojan <el.vujke@gmail.com>
2023-09-02 01:11:06 +0200

    Update mysql_backup.sh

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-#: Title		    : Backup all MySQL Databases
-#: Date			    : 2014
-#: Author		    : github.com/vujke
-#: Version		  : 2.0
-#: Description	: 
+#: Title          : Backup all MySQL Databases
+#: Date           : 2014
+#: Author         : github.com/vujke
+#: Version        : 2.0
+#: Description    : 
 
 DATE=$(date +"%d%m%Y%H%M")
 
