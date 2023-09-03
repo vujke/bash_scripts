1c79ee0c9f6c08c48e7049fd27ced7b41bd46c06
Bojan <el.vujke@gmail.com>
2023-09-03 16:00:30 +0200

    Update mysql_backup.sh

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -22,13 +22,13 @@ TIME=$(date)
 # Check if database directory exist
 [ ! -d "$BACKUP_DIR" ]  && mkdir -p $BACKUP_DIR
 
+# Delete databases older than 15 days
+find $BACKUP_DIR/$DATABASE_NAME* -mtime +15 -exec rm {} \;
+
 # List all databases
 # SHOW DATABASES is regular MySQL command that shows all databases
 # Command grep -Ev will NOT show defined database(s)
 LIST_OF_DATABASES=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys)"`
-
-# Delete databases older than 15 days
-find $BACKUP_DIR/$DATABASE_NAME* -mtime +15 -exec rm {} \;
 
 for DATABASE_NAME in $LIST_OF_DATABASES; do
   $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $DATABASE_NAME --skip-lock-tables > ${BACKUP_DIR}/${DATABASE_NAME}_${DATE}.sql
